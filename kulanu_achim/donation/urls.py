c687d4fc506fdf23a741f6365d31022bec29e0b5
YoniPel <yehonatan3000@gmail.com>
2024-01-01 18:20:06 +0200

    some updates

--- kulanu_achim/donation/urls.py
+++ kulanu_achim/donation/urls.py
@@ -4,7 +4,7 @@ from .views import donation, DonationItems, donation_success, donation_failure
 urlpatterns = [
     # path (path, what the user will see, name)
     path('donation/', donation, name='donationPage'),
-    path('items_donation/', DonationItems.as_view(), name='donationItemsPage'),
+    path('donation/items', DonationItems.as_view(), name='donationItemsPage'),
     path('donation_success/', donation_success, name='donationSuccessPage'),
     path('donation_failure/', donation_failure, name='donationFailurePage')
 ]
